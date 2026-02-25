d196a8daeb80e041a6b3f818f8543f86b6d978b9
Alex Savin <alesavin@users.noreply.github.com>
2026-02-25 19:11:55 +0100

    Add device-profile renderer tests for APL/APLT

--- lambda/clock_pet/handlers/interaction.py
+++ lambda/clock_pet/handlers/interaction.py
@@ -5,7 +5,7 @@ from ask_sdk_core.handler_input import HandlerInput
 from ask_sdk_core.utils import is_intent_name
 
 from clock_pet.i18n import message
-from clock_pet.renderer import build_aplt_render_directive
+from clock_pet.renderer import build_render_directive_for_device, device_supports_apl
 from clock_pet.speech import build_blink_ssml
 from clock_pet.state import next_state
 
@@ -19,11 +19,14 @@ class PetIntentHandler(AbstractRequestHandler):
         new_state, frame = next_state(session_attributes)
         session_attributes.update(new_state)
 
-        directive = build_aplt_render_directive(frame)
         locale = handler_input.request_envelope.request.locale
-        return (
-            handler_input.response_builder.speak(build_blink_ssml(message(locale, "pet")))
-            .add_directive(directive)
-            .set_should_end_session(False)
-            .response
+        response_builder = handler_input.response_builder.speak(
+            build_blink_ssml(message(locale, "pet"))
         )
+
+        if device_supports_apl(handler_input):
+            directive = build_render_directive_for_device(handler_input, frame)
+            if directive is not None:
+                response_builder.add_directive(directive)
+
+        return response_builder.set_should_end_session(False).response
